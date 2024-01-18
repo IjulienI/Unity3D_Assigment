2a690b00f2d283456201141d3d3ee54e5c526a07
julien_ssm <89165787+IjulienI@users.noreply.github.com>
2024-01-18 21:53:11 +0100

    [rework] interaction gamefeel

---
+++ Assets/Scripts/Interactible.cs
@@ -0,0 +1,13 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Interactible : MonoBehaviour
+{
+    [SerializeField] private float distance;
+
+    public float GetDistance()
+    {
+        return distance;
+    }
+}
--- Assets/Scripts/Player/interact.cs
+++ Assets/Scripts/Player/interact.cs
@@ -1,6 +1,7 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 interface IInteractible
 {
@@ -9,23 +10,49 @@ interface IInteractible
 
 public class interact : MonoBehaviour
 {
-    [SerializeField] private float distance;
     [SerializeField] private Camera cam;
+    [SerializeField] private Image indicator;
+    private float distance = 1000;
 
     private RaycastHit hit;
 
     private void Update()
     {
-        if(Input.GetKeyDown(KeyCode.E) && CharacterController.instance.CanInteract())
+        if (Physics.Raycast(cam.transform.position, cam.transform.forward, out hit, distance))
         {
-            if (Physics.Raycast(cam.transform.position, cam.transform.forward, out hit, distance))
+            if(hit.collider.gameObject.TryGetComponent(out IInteractible obj))
             {
-                Debug.DrawLine(cam.transform.position, hit.point, Color.green, 10f);
-                if (hit.collider.gameObject.TryGetComponent(out IInteractible obj))
+                if (hit.collider.gameObject.GetComponent<Interactible>().GetDistance() >= hit.distance)
                 {
-                    obj.Interact();
+                    if (Input.GetKeyDown(KeyCode.E) && CharacterController.instance.CanInteract())
+                    {
+                        obj.Interact();
+                    }
+                    else
+                    {
+                        IndicatorState(false);
+                    }
+                    Debug.DrawLine(cam.transform.position, hit.point, Color.green, .1f);
+                    IndicatorState(true);
+                }
+                else
+                {
+                    IndicatorState(false);
                 }
             }
+            else
+            {
+                IndicatorState(false);
+            }
         }
+        else
+        {
+            IndicatorState(false);
+        }
+    }
+
+    private void IndicatorState(bool state)
+    {
+        indicator.enabled = state;
     }
 }
